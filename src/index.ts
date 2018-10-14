452f71a8984b0174fa89dddac4786b90a5cd7179
James Didehvar <james@didehvar.com>
2018-10-14 21:27:40 +0100

    nodemon, env, logger

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
+import * as cors from '@koa/cors';
+import 'dotenv/config';
 import * as Koa from 'koa';
+import * as logger from 'koa-logger';
 
 const app = new Koa();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger());
+}
+
+app.use(
+  cors({
+    origin: process.env.APP_URL,
+  }),
+);
 
 app.use(async ctx => {
   ctx.body = 'Hello World';
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
