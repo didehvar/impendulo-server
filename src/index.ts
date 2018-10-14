386a85cc1593d3ba2a2c080f2c5dced5ea8ff326
James Didehvar <james@didehvar.com>
2018-10-14 23:26:40 +0100

    use https locally

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as cors from '@koa/cors';
 import 'dotenv/config';
+import * as fs from 'fs';
+import * as https from 'https';
 import * as Koa from 'koa';
 import * as logger from 'koa-logger';
 
@@ -11,6 +13,8 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(
   cors({
+    allowHeaders: 'Authorization',
+    allowMethods: 'GET,POST',
     origin: process.env.APP_URL,
   }),
 );
@@ -19,4 +23,16 @@ app.use(async ctx => {
   ctx.body = 'Hello World';
 });
 
-app.listen(process.env.PORT || 3000);
+if (process.env.NODE_ENV === 'development') {
+  https
+    .createServer(
+      {
+        cert: fs.readFileSync('keys/local.crt'),
+        key: fs.readFileSync('keys/local.key'),
+      },
+      app.callback(),
+    )
+    .listen(process.env.PORT);
+} else {
+  app.listen(process.env.PORT);
+}
